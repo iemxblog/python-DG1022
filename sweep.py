1ab29d96936860950f938a352ab73bdef6ab5f4c
iemxblog <iemxblog@gmail.com>
2017-03-03 16:00:51 +0100

    Cleaner timestamp code in the example

--- sweep.py
+++ sweep.py
@@ -2,15 +2,17 @@ from DG1022 import DG1022
 import time
 import datetime
 
-memtime = time.time()
-def timestamp():
-    global memtime
-    print(str(datetime.datetime.now()))
-    print("diff = %.2f s" % (time.time() - memtime))
-    memtime = time.time()
 
+class Timestamp:
+    def __init__(self):
+        self.memtime = time.time()
 
-f1 = 0.5 
+    def display(self):
+        print(str(datetime.datetime.now()))
+        print("diff = %.2f s" % (time.time() - self.memtime))
+        self.memtime = time.time()
+  
+f1 = 0.5
 f2 = 121
 step = 0.01
 pulse_time = 0.4
@@ -18,7 +20,6 @@ pause_time = 0.6
 repetitions = 5
 freq_change_time = 0.1
 vpp = 20.0
-
 
 def frequencies():
     f = f1
@@ -30,13 +31,14 @@ def frequencies():
 print("It will take %f hours" % ((f2-f1) / step * repetitions * (pulse_time + pause_time) /3600))
 
 gen = DG1022('/dev/usbtmc0')
+timestamp = Timestamp()
 gen.sinusoid()
 time.sleep(0.1)
 gen.voltage(vpp)
 time.sleep(0.1)
 
 
-timestamp()
+timestamp.display()
 print("Beginning sweep")
 
 for f in frequencies():
@@ -44,11 +46,11 @@ for f in frequencies():
     gen.frequency(f)
     time.sleep(freq_change_time)
     for i in range(5):
-        timestamp()
+        timestamp.display()
         print("ON")
         gen.output(True)
         time.sleep(pulse_time)
-        timestamp()
+        timestamp.display()
         print("OFF")
         gen.output(False)
         if i < 4:
@@ -56,5 +58,5 @@ for f in frequencies():
         else:
             time.sleep(pause_time - freq_change_time)
 
-timestamp()
+timestamp.display()
 print("Sweep terminated")
