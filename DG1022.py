8e0b1008f81cb7748feaf6211507f4ae2d8342a5
iemxblog <iemxblog@gmail.com>
2017-10-22 12:34:39 +0200

    Refactored code + offset + pos_voltage

--- DG1022.py
+++ DG1022.py
@@ -1,27 +1,36 @@
 import os
+import time
 
 class DG1022:
     def __init__(self, device):
         self.device = device
-        self.f = os.open('/dev/usbtmc0', os.O_RDWR)
+        self.f = os.open(device, os.O_RDWR)
 
     def output(self, state):
         if state == True:
-            os.write(self.f, "OUTP ON")
+            self.write("OUTP ON")
         else:
-            os.write(self.f, "OUTP OFF")
+            self.write("OUTP OFF")
 
     def frequency(self, freq):
-        os.write(self.f, "FREQ %.3f" % freq)
+        self.write("FREQ %.3f" % freq)
 
     def sinusoid(self):
-        os.write(self.f, "APPL:SIN")
+        self.write("APPL:SIN")
 
     def voltage(self, vpp):
-        os.write(self.f, "VOLT %.2f" % vpp)
+        self.write("VOLT %.3f" % vpp)
+
+    def offset(self, o):
+        self.write("VOLT:OFFS %.3f" % o)
+    
+    def pos_voltage(self, vpp):
+        self.voltage(vpp)
+        self.offset(vpp/2) 
 
     def read(self, length=4000):
         return os.read(self.f, length)
 
     def write(self, cmd):
         os.write(self.f, cmd)
+        time.sleep(0.1)
